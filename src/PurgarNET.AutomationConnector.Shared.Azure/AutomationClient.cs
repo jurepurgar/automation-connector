304ed0ca4f55e777c597244649a7e6f50ed38a29
Jure Purgar <jure@purgar.net>
2017-03-16 12:46:22 +0100

    Added shared libs

---
+++ src/PurgarNET.AutomationConnector.Shared.Azure/AutomationClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurgarNET.AutomationConnector.Shared.Azure
+{
+    public class AutomationClient : AutomationClientBase
+    {
+    }
+}
