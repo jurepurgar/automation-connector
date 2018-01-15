bc9d460508775f4294371cd668aff6734d2701e7
Jure Purgar <jure@purgar.net>
2018-01-15 12:14:07 +0100

    commit

---
+++ src/PurgarNET.AutomationConnector.Console/ManageConnectorTask.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EnterpriseManagement.ConsoleFramework;
+using Microsoft.EnterpriseManagement.UI.SdkDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PurgarNET.AutomationConnector.Console
+{
+    public class ManageConnectorTask : ConsoleCommand
+    {
+        public override void ExecuteCommand(IList<NavigationModelNodeBase> nodes, NavigationModelNodeTask task, ICollection<string> parameters)
+        {
+            base.ExecuteCommand(nodes, task, parameters);
+
+            string nodesStr = "Nodes:";
+            foreach (NavigationModelNodeBase node in nodes)
+            {
+                nodesStr += $"{node.DisplayName}/r/n";
+            }
+
+            string parametersStr = "Parameters:";
+            foreach (var p in parameters)
+            {
+                parametersStr += p;
+            }
+
+
+            var str = $@"
+{nodesStr}
+Task:  {task.DisplayName}
+{parametersStr}
+
+";
+
+            MessageBox.Show(str);
+           
+
+        }
+    }
+}
