f0a96c6d2cb61ff03b768d867a902869b140af10
Jure Purgar <jure@purgar.net>
2018-01-15 16:30:08 +0100

    commit

--- src/PurgarNET.AutomationConnector.Console/AzureConnectorViewModel.cs
+++ src/PurgarNET.AutomationConnector.Console/AzureConnectorViewModel.cs
@@ -10,6 +10,12 @@ namespace PurgarNET.AutomationConnector.Console
     {
         public AzureConnectorViewModel(bool isNew) : base(isNew)
         {
+            Properties.Add(new Property() { Name = "TenantId", DisplayName = "Tenant ID" });
+            Properties.Add(new Property() { Name = "SubscriptionId", DisplayName = "Subscription ID" });
+            Properties.Add(new Property() { Name = "ResourceGroupName", DisplayName = "Resource Group Name" });
+            Properties.Add(new Property() { Name = "AutomationAccountName", DisplayName = "Automation Account Name ID" });
+            Properties.Add(new Property() { Name = "AppId", DisplayName = "Service principal ID" });
+            Properties.Add(new Property() { Name = "Pass", DisplayName = "Service principal key" });
         }
     }
 }
